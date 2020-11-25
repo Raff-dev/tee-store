d207cfaf29156c45d29a31532b56971b8bd2e4b9
Raff-dev <56380303+Raff-dev@users.noreply.github.com>
2020-11-25 14:30:37 +0100

    changinch cms architecture

--- VapeShop/ClientApp/src/components/Pages/Admin/Admin.js
+++ VapeShop/ClientApp/src/components/Pages/Admin/Admin.js
@@ -1,59 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { LinkContainer } from 'react-router-bootstrap';
-import { Button } from '@material-ui/core';
-import { Route, Switch } from 'react-router-dom';
-
-import { Resource } from '../../utilities/Resource';
-import { Sidebar } from './Sidebar';
-import { DataTable } from './DataTable';
-
-import ModelForm from './ModelForm';
-import DeletePrompt from './DeletePrompt';
+import { Resource } from '../../utilities/Resource'
+import { ProductsManager } from './ProductsManager/ProductsManager'
 
 const Admin = () => {
-  const [model, setModel] = useState(null);
-  const models = ['Users', 'Categories', 'Products', 'Reviews'];
-
   return (
     <section className="admin" >
-      <Sidebar models={models} setModel={setModel} />
-      {model && <Resource model={model} path={`api/${model}`} render={Manager} />}
+      <Resource path={`api/Categories`} render={ProductsManager} />
     </section>
   );
-}
-
-const Manager = ({ model, payload, loading }) => {
-
-  function findInstanceById(id) {
-    return payload.filter(instance => instance['id'] === parseInt(id));
-  }
-
-  return (loading)
-    ? <p >Loading...</p>
-    : (
-      <div>
-        <div className="option-header d-flex align-middle p-2">
-          <span className="m-4">{model}</span>
-          <LinkContainer to={`Create`}>
-            <Button className="my-3 " variant="contained">Add New</Button>
-          </LinkContainer>
-        </div>
-        <Switch>
-          <Route path={`/Admin/:model/Create`} component={props =>
-            <ModelForm {...props} />} />
-
-          <Route path={`/Admin/:model/Read`} component={props =>
-            <DataTable entries={payload} {...props} />} />
-
-          <Route path={`/Admin/:model/Delete/:id`} component={props =>
-            <DeletePrompt instance={findInstanceById(props.match.params.id)[0]} {...props} />} />
-
-          <Route path={`/Admin/:model/Update/:id`} component={props =>
-            <ModelForm instance={findInstanceById(props.match.params.id)[0]} {...props} />} />
-        </Switch>
-      </div >
-    );
 }
 
 export default Admin;
--- VapeShop/ClientApp/src/components/Pages/Admin/Forms/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () => {
-    return (
-        <p>No such model form was found</p>
-    );
-}
-
-export default NotFound;
