fd14085869751e4ca272f79073d582dee4044e50
Raff <rafal.lazicki99@gmail.com>
2021-02-25 19:56:05 +0100

    update settings

--- VapeShop/api/api/settings.py
+++ VapeShop/api/api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .secret import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')te-%do4d0ml%glgmo=gfgk=1aa_wipv)@re7)*)-76&lhn&e1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party
+    'rest_framework',
+    # local apps
+    'products.apps.ProductsConfig',
+
 ]
 
 MIDDLEWARE = [
