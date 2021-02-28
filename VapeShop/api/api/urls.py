3b8aa02db8d20eef35d482a6aab2e1bbd9cc0feb
Raff <rafal.lazicki99@gmail.com>
2021-02-28 15:52:18 +0100

    add static files to urls

--- VapeShop/api/api/urls.py
+++ VapeShop/api/api/urls.py
@@ -2,7 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from django.conf.urls.static import static
+from django.conf import settings
 from products.views import ProductViewSet, CategoryViewSet
 
 router = DefaultRouter()
@@ -16,7 +17,7 @@ urlpatterns = [
     path('api/', include((router.urls, 'Categories'), namespace='Categories')),
     path('api/', include((router.urls, 'Products'), namespace='Products')),
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 for url in router.urls:
     print(url)
