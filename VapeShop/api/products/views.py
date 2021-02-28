271e3a3621dfba9ef44540589e166c670c701f94
Raff <rafal.lazicki99@gmail.com>
2021-02-28 16:30:07 +0100

    create Product, Category viewsets

--- VapeShop/api/products/views.py
+++ VapeShop/api/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Category, Product
+from .serializers import ProductSerializer, CategorySerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    @action(methods=['get'], detail=False)
+    def get_categories(self, request, *args, **kwargs):
+        category_name = kwargs['category']
+        category = Category.objects.filter(name=category_name).first()
+        products = self.queryset.filter(category=category)
+
+        prorducts_serialized = products
+        return Response(data=prorducts_serialized, status=status.HTTP_200_OK)
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
