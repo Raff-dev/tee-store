4f187c25b122f709e75ff98683e99df5560685b8
Raff <rafal.lazicki99@gmail.com>
2021-03-01 00:38:22 +0100

    add model serializers

--- VapeShop/api/products/serializers.py
+++ VapeShop/api/products/serializers.py
@@ -1,14 +1,62 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Image, Product, Category, Collection, Size, Variant
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class SizeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Size
+        exclude = ['variant']
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        exclude = ['variant']
+
+
+class VariantListSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Variant
+        exclude = ['product']
+
+
+class VariantDetailSerializer(VariantListSerializer):
+    sizes = SizeSerializer(many=True, read_only=True)
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    variants = VariantDetailSerializer(many=True, read_only=True)
+    category = serializers.StringRelatedField(many=False)
+    collection = serializers.StringRelatedField(many=False)
+
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ['created_at', 'updated_at']
+
+
+class ProductListSerializer(ProductDetailSerializer):
+    variants = VariantListSerializer(many=True, read_only=True)
+
+    class Meta(ProductDetailSerializer.Meta):
+        exclude = ProductDetailSerializer.Meta.exclude + [
+            'title', 'material', 'description']
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
+
     class Meta:
         model = Category
-        fields = '__all__'
+        exclude = ['id']
+        lookup_field = 'name'
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Collection
+        exclude = ['id']
+        lookup_field = 'name'
