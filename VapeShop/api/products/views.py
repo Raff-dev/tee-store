19facde16b39a03a8a74dea4eff4dea9bfea71bf
Raff <rafal.lazicki99@gmail.com>
2021-03-01 21:32:34 +0100

    update remove unnecessary imports

--- VapeShop/api/products/views.py
+++ VapeShop/api/products/views.py
@@ -1,5 +1,4 @@
-from rest_framework import mixins, serializers, status, viewsets
-from rest_framework.decorators import action
+from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Category, Product, Collection
 from .serializers import CollectionSerializer, ProductListSerializer, ProductDetailSerializer, CategorySerializer
