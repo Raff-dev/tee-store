e3a5cde67c4bca782458627e1b63335ddca42ac5
Raff <rafal.lazicki99@gmail.com>
2021-03-01 20:43:52 +0100

    simplify index

--- VapeShop/ClientApp/src/Index.js
+++ VapeShop/ClientApp/src/Index.js
@@ -13,11 +13,7 @@ import Admin from './components/Pages/Admin/Admin'
 import Cart from './components/Pages/Cart/Cart'
 import Footer from './components/Footer/Footer'
 import NavMenu from './components/NavMenu/NavMenu'
-
-import { Category } from './components/Pages/Home/Category/Category'
-import { Product } from './components/Pages/Home/Product/Product'
 import { ToastProvider } from 'react-toast-notifications'
-import { Resource } from './components/utilities/Resource'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -28,26 +24,6 @@ import './index.scss';
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
-const CategoryRender = ({ match }) => {
-  const { category } = match.params;
-
-  return (
-    <Resource path={`api/Products/OfCategory/${category}`}>
-      {props => <Category category={category} {...props} />}
-    </Resource>
-  );
-}
-
-const ProductsRender = ({ match }) => {
-  const { id } = match.params;
-
-  return (
-    <Resource path={`api/Products/${id}`}>
-      {props => <Product id={id} {...props} />}
-    </Resource>
-  );
-}
-
 ReactDOM.render(
   <BrowserRouter basename={baseUrl} >
     <ToastProvider>
@@ -55,8 +31,6 @@ ReactDOM.render(
       <Grid>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/Category/:category' component={CategoryRender} />
-          <Route path='/Product/:id' component={ProductsRender} />
           <Route path='/Cart' component={Cart} />
           <Route path='/Admin' component={Admin} />
         </Switch>
