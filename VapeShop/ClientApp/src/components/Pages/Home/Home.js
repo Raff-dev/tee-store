95722e578dbcd78548bc069c8dfb4723be34a57c
Raff <rafal.lazicki99@gmail.com>
2021-03-03 00:01:57 +0100

    update home layout

--- VapeShop/ClientApp/src/components/Pages/Home/Home.js
+++ VapeShop/ClientApp/src/components/Pages/Home/Home.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import { useState } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap'
+
 import { PromotionCarousel } from './PromotionCarousel'
 import { CollectionButtonGroup } from './CollectionButtonGroup'
 import { CategoryButtonGroup } from './CategoryButtonGroup'
 import { ProductsMesh } from './ProductsMesh'
-import './Home.scss';
+import { Resource } from '../../utilities/Resource';
+import { Loadable } from '../../utilities/Loadable';
 
 const Home = (props) => {
-  const [category, setCategory] = useState(null);
+  const allCategory = 'All products';
+  const apiUrl = 'http://127.0.0.1:8000/api/'
+
+  const [category, setCategory] = useState(allCategory);
   const [collection, setCollection] = useState(null);
 
+
   return (
-    <div>
-      <PromotionCarousel />
-      <br />
-      <CollectionButtonGroup setCollection={setCollection} />
-      <Grid>
-        <Row>
-          <Col>
-            <CategoryButtonGroup setCategory={setCategory} />
-          </Col>
-          <Col>
-            <ProductsMesh category={category} collection={collection} />
-          </Col>
-        </Row>
-      </Grid>
-    </div>
+    <Resource path={apiUrl + 'Products'}>
+      {({ payload, loading }) => {
+        let { products, categories, collections } = payload;
+        return (
+          <section>
+            <PromotionCarousel />
+            <br />
+            <Loadable>
+              <CollectionButtonGroup setCollection={setCollection} collections={collections} />
+            </Loadable>
+            <Grid container direction="row">
+              <Col>
+                <Loadable>
+                  <CategoryButtonGroup setCategory={setCategory} categories={categories && [allCategory, ...categories] || []} />
+                </Loadable>
+              </Col>
+              <Col>
+                <Loadable loading={loading}>
+                  <ProductsMesh products={products} category={category} />
+                </Loadable>
+              </Col>
+            </Grid>
+          </section>
+        )
+      }}
+    </Resource >
   );
 }
 
