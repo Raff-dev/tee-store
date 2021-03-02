75819db69e8cb7959bc34e4a6879cb5caa22570c
Raff <rafal.lazicki99@gmail.com>
2021-03-03 00:03:16 +0100

    Update CategoryButtonGroup.js

--- VapeShop/ClientApp/src/components/Pages/Home/CategoryButtonGroup.js
+++ VapeShop/ClientApp/src/components/Pages/Home/CategoryButtonGroup.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import styled from 'styled-components'
 
-export const CategoryButtonGroup = ({ setCategory }) => {
+export const CategoryButtonGroup = ({ setCategory, categories }) => {
     return (
-        <div>CategoryButtonGroup</div>
+        <Container>
+            {categories.map((category, index) =>
+                <Category key={index} onClick={() => setCategory(category)}>
+                    {category}
+                </Category>
+            )}
+        </Container>
     );
 }
 
+const Category = styled.span`
+    font-size: 1.1rem;
+    font-weight:600;
+    &:hover {
+        opacity:0.7;
+        cursor:pointer;
+        color: #c483e9;
 
+    }
+`;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
