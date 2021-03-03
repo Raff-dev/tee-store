b7760b3cb515c5bb68b87ee8b14962afb3736ac2
Raff <rafal.lazicki99@gmail.com>
2021-03-04 00:40:37 +0100

    update styling

--- VapeShop/ClientApp/src/components/Pages/ProductDetail/ImagePreview.js
+++ VapeShop/ClientApp/src/components/Pages/ProductDetail/ImagePreview.js
@@ -1,34 +1,46 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-export const ImagePreview = ({ variant }) => {
+export const ImagePreview = ({ product, variant }) => {
     const apiUrl = 'http://127.0.0.1:8000';
     const [imageIndex, setImageIndex] = useState(0);
 
     return (
-        <ImagesContainer>
-            {variant.images.map((image, index) =>
-                <MiniatureImage
-                    key={index}
-                    src={apiUrl + image.image}
-                    onClick={() => setImageIndex(index)}
-                />
-            )}
-            <CurrentImage src={apiUrl + variant.images[imageIndex].image} />
-        </ImagesContainer>
+        <section>
+            <Name>
+                {product.name}
+            </Name>
+            <ImagesContainer>
+                {variant.images.map((image, index) =>
+                    <MiniatureImage
+                        key={index}
+                        src={apiUrl + image.image}
+                        onClick={() => setImageIndex(index)}
+                        selected={imageIndex == index}
+                    />
+                )}
+                <CurrentImage src={apiUrl + variant.images[imageIndex].image} />
+            </ImagesContainer>
+        </section>
     );
 }
 
+const Name = styled.div`
+    font-size:1.5rem;
+    font-weight:600;
+    color:rgba(0,0,0,0.7);
+    padding-bottom:20px;
+`;
 const CurrentImage = styled.img`
 
 `;
 const MiniatureImage = styled.img`
     height:60px;
     border-radius:5px;
-    border:1px solid rgba(100, 100, 100, 0.5);
+    border:1px solid;
+    border-color: ${props => (props.selected ? "#0008" : "#0002")} ;
 
     &:hover{
-        border-color: rgba(100, 100, 100, 0.2);
         cursor: pointer;
     }
 
