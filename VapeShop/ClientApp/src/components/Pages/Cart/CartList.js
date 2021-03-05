62c220a4eadc3719ec1253bea2f945216cbc3fda
Raff <rafal.lazicki99@gmail.com>
2021-03-05 16:05:43 +0100

    Update CartList.js

--- VapeShop/ClientApp/src/components/Pages/Cart/CartList.js
+++ VapeShop/ClientApp/src/components/Pages/Cart/CartList.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { CartProduct } from './CartProduct'
 
+import { Resource } from '../../utilities/Resource';
+
+
 export const CartList = ({ cartProducts }) => {
+
+
     return (
         <div>CART PRODUCTS</div>
+
+
     );
 }
