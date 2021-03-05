b91ecaa98f68daeca1bf95dbc3ca38879ef5a4b4
Raff <rafal.lazicki99@gmail.com>
2021-03-05 16:05:32 +0100

    use cart context and api context

--- VapeShop/ClientApp/src/components/Pages/Cart/Cart.js
+++ VapeShop/ClientApp/src/components/Pages/Cart/Cart.js
@@ -1,27 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Col, Row } from 'react-bootstrap';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { Grid, Col, Row } from 'react-bootstrap';
 
+import { CartContext } from '../../../contexts/CartContext'
+import { ApiContext } from '../../../contexts/ApiContext'
 import { CartList } from './CartList'
 import { CartMenu } from './CartMenu'
+import { Resource } from '../../utilities/Resource';
 
 const Cart = () => {
-    return (
-        <Grid>
-            <Row>
-                <Title>YOUR CART</Title>
-                <Col lg={8} md={12}>
-                    <CartList />
-                </Col>
-                <Col lg={4} md={12}>
-                    <CartMenu />
-                </Col>
-            </Row>
-            <Row>
-                PROPOSED ITEMS
-            </Row>
+    const api = useContext(ApiContext);
+    const cart = useContext(CartContext);
+    const ids = { ids: cart.ids };
 
-        </Grid>
+    return (
+        <Resource path={api.cart} data={ids} disabled={!!ids === false}>
+            {({ payload, loading }) => {
+                console.log(payload)
+                console.log(cart)
+                return (
+                    <Grid>
+                        <Row>
+                            <Title>YOUR CART</Title>
+                            <Col lg={8} md={12}>
+                                <CartList />
+                            </Col>
+                            <Col lg={4} md={12}>
+                                <CartMenu />
+                            </Col>
+                        </Row>
+                        <Row>
+                            PROPOSED ITEMS
+                        </Row>
+                    </Grid>
+                );
+            }}
+        </Resource>
     );
 };
 
@@ -29,6 +43,5 @@ const Title = styled.span`
     font-size: 1.2rem;
     font-weight:500;
 `;
-
 
 export default Cart;
