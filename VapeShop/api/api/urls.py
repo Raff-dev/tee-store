ff262c0947ba062e49c2d9c3031bd30d19e47f9f
Raff <rafal.lazicki99@gmail.com>
2021-03-05 16:03:04 +0100

    register size viewset

--- VapeShop/api/api/urls.py
+++ VapeShop/api/api/urls.py
@@ -4,12 +4,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import static
 from django.conf import settings
-from products.views import CollectionViewSet, ProductViewSet, CategoryViewSet
+from products.views import CollectionViewSet, ProductViewSet, CategoryViewSet, SizeViewSet
 
 router = DefaultRouter()
 router.register('Categories', CategoryViewSet)
 router.register('Collections', CollectionViewSet)
 router.register('Products', ProductViewSet)
+router.register('Sizes', SizeViewSet)
 
 # The API URLs are now determined automatically by the router.
 
@@ -18,9 +19,9 @@ urlpatterns = [
     path('api/', include((router.urls, 'Categories'), namespace='Categories')),
     path('api/', include((router.urls, 'Collections'), namespace='Collections')),
     path('api/', include((router.urls, 'Products'), namespace='Products')),
+    path('api/', include((router.urls, 'Sizes'), namespace='Sizes')),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
-
 
 for url in router.urls:
     print(url)
