de3668a69bf007fc7d448fb09f42c6006fd0afab
Raff <rafal.lazicki99@gmail.com>
2021-03-05 16:03:36 +0100

    Create ApiContext.js

---
+++ VapeShop/ClientApp/src/contexts/ApiContext.js
@@ -0,0 +1,24 @@
+import React, { useState, createContext, useEffect } from 'react'
+
+export const ApiContext = createContext();
+
+export const ApiProvider = ({ children }) => {
+    const baseUrl = 'http://127.0.0.1:8000';
+    const apiUrl = 'http://127.0.0.1:8000/api/';
+    const api = {
+        baseUrl: baseUrl,
+        apiUrl: apiUrl,
+        products: apiUrl + 'Products/',
+        sizes: apiUrl + 'Sizes/',
+        cart: apiUrl + 'Sizes/cart_products/',
+        categories: apiUrl + 'Categories/',
+        collections: apiUrl + 'Collections/',
+    }
+
+    return (
+        <ApiContext.Provider value={api}>
+            {children}
+        </ApiContext.Provider>
+    );
+
+}
