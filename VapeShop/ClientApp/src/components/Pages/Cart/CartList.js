3a0148ad240eca98561a86f7c00853140968dfa1
Raff <rafal.lazicki99@gmail.com>
2021-03-06 17:05:04 +0100

    Update CartList.js

--- VapeShop/ClientApp/src/components/Pages/Cart/CartList.js
+++ VapeShop/ClientApp/src/components/Pages/Cart/CartList.js
@@ -1,15 +1,77 @@
 import React, { useContext } from 'react';
-import { CartProduct } from './CartProduct'
 
-import { Resource } from '../../utilities/Resource';
+import styled from 'styled-components';
+import { ButtonGroup, Button } from '@material-ui/core';
 
+import { ApiContext } from '../../../contexts/ApiContext'
+import { CartContext } from '../../../contexts/CartContext'
 
 export const CartList = ({ cartProducts }) => {
-
+    const api = useContext(ApiContext);
+    const cart = useContext(CartContext);
 
     return (
-        <div>CART PRODUCTS</div>
+        <section>
+            {cartProducts.map((product, index) =>
+                <ListEntry>
+                    <ProductDisplay>
+                        <ProductImage src={api.baseUrl + product.image} />
+                        <div>
+                            <p>{product.collection}</p>
+                            <p>{product.title}</p>
+                            <p>${product.properties}</p>
+                        </div>
+                    </ProductDisplay>
 
+                    <QuantityMenu>
+                        <ButtonGroup variant="contained" color="primary" >
+                            <Button onClick={() => {
+                                cart.updateItem(product.id, cart.quantityMap[product.id] - 1)
+                            }}>
+                                -
+                            </Button>
+                            <Input
+                                type="number"
+                                value={cart.quantityMap[product.id]}
+                            />
+                            <Button onClick={() => cart.addItem(product.id)}>
+                                +
+                            </Button>
+                        </ButtonGroup>
+                        <RemoveButton onClick={() => cart.removeItem(product.id)}>
+                            Remove
+                        </RemoveButton>
+                    </QuantityMenu>
+                </ListEntry>
+            )}
 
+        </section>
     );
 }
+
+const RemoveButton = styled.div`
+    :hover{
+        cursor: pointer;
+        text-decoration:underline;
+    }
+`;
+
+const ListEntry = styled.div`
+    display:flex;
+    justify-content:space-between
+`;
+
+const ProductImage = styled.img`
+    width:100px;
+
+`;
+const ProductDisplay = styled.div`
+    display: flex;
+`;
+
+
+const Input = styled.input`
+`;
+
+const QuantityMenu = styled.div`
+`;
