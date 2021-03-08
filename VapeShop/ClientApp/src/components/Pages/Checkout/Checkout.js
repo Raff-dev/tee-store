8ac8c2682395d59ed0ea33182e6bdb3d66c47284
Raff <rafal.lazicki99@gmail.com>
2021-03-08 13:05:40 +0100

    move submit logic to CheckoutForm

--- VapeShop/ClientApp/src/components/Pages/Checkout/Checkout.js
+++ VapeShop/ClientApp/src/components/Pages/Checkout/Checkout.js
@@ -2,36 +2,25 @@ import React, { useContext, useState } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import { Breadcrumbs, Typography } from '@material-ui/core';
-import { ApiContext } from '../../../contexts/ApiContext';
-import { CartContext } from '../../../contexts/CartContext';
-import { Shipping } from './Shipping'
+
+import { CheckoutForm } from './CheckoutForm'
 import { CheckoutSummary } from './CheckoutSummary'
 
 const Checkout = () => {
-    const api = useContext(ApiContext);
-    const cart = useContext(CartContext);
-
-    const onSubmit = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const formDataObj = Object.fromEntries(formData.entries())
-        console.log(formDataObj)
-    }
-
     return (
-        <Grid>
-            <Col md={12} lg={6}>
-                <Row>
+        <Grid fluid className="p-0 mx-4">
+            <Col md={12} lg={6} >
+                <Row className="ml-2">
                     <Breadcrumbs>
                         <Link to='/Cart'>Cart</Link>
                         <Typography >Shipping</Typography>
                     </Breadcrumbs>
                 </Row>
-                <Row>
-                    <Shipping onSubmit={onSubmit} />
+                <Row className="ml-2">
+                    <CheckoutForm />
                 </Row>
             </Col>
-            <Col md={12} lg={6} style={{ "background-color": "#0001", "height": "800px" }}>
+            <Col md={12} lg={6} style={{ "background-color": "#0001", "height": "1000px" }}>
                 <CheckoutSummary />
             </Col>
         </Grid>
