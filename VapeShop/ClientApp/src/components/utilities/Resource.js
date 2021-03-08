e03ef570fc7a129c2021ca3fa836ffffbb8a209e
Raff <rafal.lazicki99@gmail.com>
2021-03-08 13:06:34 +0100

    enable post through resource component

--- VapeShop/ClientApp/src/components/utilities/Resource.js
+++ VapeShop/ClientApp/src/components/utilities/Resource.js
@@ -2,18 +2,20 @@ import { useState, useEffect } from 'react';
 import axios from 'axios'
 
 
-export const Resource = ({ path, children, ...props }) => {
+export const Resource = ({ path, data, disabled, children, ...props }) => {
     const [loading, setLoading] = useState(true);
     const [payload, setPayload] = useState([]);
     const [refreshVar, setRefreshVar] = useState(false);
-    const [data, setData] = useState(null);
 
     const refresh = () => {
         setRefreshVar(v => !v)
     }
 
     useEffect(() => {
-        axios.get(path, data)
+        console.log('disabled' + disabled);
+        if (disabled) return;
+        let func = data ? axios.post : axios.get;
+        func(path, data)
             .then(res => {
                 setPayload(res.data)
                 setLoading(false)
@@ -21,5 +23,5 @@ export const Resource = ({ path, children, ...props }) => {
             .catch(console.log)
     }, [path, refreshVar, data]);
 
-    return children({ loading, payload, refresh, setData, ...props });
+    return children({ loading, payload, refresh, ...props });
 };
