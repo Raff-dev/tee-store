e8368551bc5a2811db3990520096db7a1eff0dee
Raff <rafal.lazicki99@gmail.com>
2021-03-09 23:54:01 +0100

    add theme colors

--- VapeShop/ClientApp/src/contexts/ThemeContext.js
+++ VapeShop/ClientApp/src/contexts/ThemeContext.js
@@ -2,18 +2,28 @@ import React, { createContext } from 'react'
 
 export const ThemeContext = createContext();
 
+export const theme = {
+    borderColor: "",
+    primary: "",
+    secondary: "",
+    bgPrimary: "",
+    bgSecondary: "",
+
+    btnPrimary: "#c60021",
+    btnTextPrimary: "#fefefe",
+    btnSecondary: "#f1f5f8",
+    btnTextSecondary: "#3d4852",
+    btnDisabled: "rgb(50,230,50)",
+    btnTextDisabled: "",
+
+    textDefault: "rgb(61, 72, 82)",
+    textHighlight: "#c60021",
+    textMuted: "rgb(135, 149, 161)",
+    textInfo: "rgb(61, 72, 82)",
+
+}
+
 export const ThemeProvider = ({ children }) => {
-    const theme = {
-        borderColor: "",
-        primary: "",
-        secondary: "",
-        bgPrimary: "",
-        bgSecondary: "",
-        success: "",
-        info: "",
-        disabled: "",
-        danger: "",
-    };
 
     return (
         <ThemeContext.Provider value={theme}>
