9c965fbf121a25ec75cea1d03e6bf1c189e7899f
Raff <rafal.lazicki99@gmail.com>
2021-03-09 23:54:26 +0100

    fix layout

--- VapeShop/ClientApp/src/components/Pages/Home/CategoryButtonGroup.js
+++ VapeShop/ClientApp/src/components/Pages/Home/CategoryButtonGroup.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 export const CategoryButtonGroup = ({ setCategory, categories }) => {
     return (
         <Container>
+            <p>Filter by product</p>
             {categories.map((category, index) =>
                 <Category key={index} onClick={() => setCategory(category)}>
                     {category}
@@ -16,6 +17,7 @@ export const CategoryButtonGroup = ({ setCategory, categories }) => {
 const Category = styled.span`
     font-size: 1.1rem;
     font-weight:600;
+    padding-left:10px;
     &:hover {
         opacity:0.7;
         cursor:pointer;
@@ -27,4 +29,6 @@ const Category = styled.span`
 const Container = styled.div`
     display: flex;
     flex-direction: column;
+    width:auto;
+    padding:20px;
 `;
--- VapeShop/ClientApp/src/components/Pages/Home/ProductsMesh.js
+++ VapeShop/ClientApp/src/components/Pages/Home/ProductsMesh.js
@@ -6,22 +6,25 @@ import { ProductCard } from './ProductCard'
 
 export const ProductsMesh = ({ products, category }) => {
     return (
-        <Mesh>
-            <Row>
-                {products.map((product, _) =>
+        <Mesh xs={1} md={3} lg={4}>
+            {products.map((product, _) => {
+                return (
                     (category === 'All products' || product.category === category) &&
                     product.variants.map((variant, index) =>
-                        <Col xs={6} md={4}>
-                            <ProductCard product={product} variant={variant} key={variant.id} />
+                        <Col >
+                            <ProductCard className="flex-wrap" product={product} variant={variant} key={variant.id} />
                         </Col>
                     )
-                )}
-            </Row>
+                )
+            }
+            )}
         </Mesh>
     );
 }
 
-const Mesh = styled(Grid)`
+const Mesh = styled(Row)`
     min-height:100vh;
+    display:flex;
+    justify-content:center;
 
 `;
