9f3205a9792a839d47b7f42b23dab2195622f7a8
Raff <rafal.lazicki99@gmail.com>
2021-03-09 20:37:36 +0100

    remove collection buttons

--- VapeShop/ClientApp/src/components/Pages/Home/CollectionButtonGroup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const CollectionButtonGroup = ({ setCollection }) => {
-    return (
-        <div>CollectionButtonGroup</div>
-    );
-}
-
--- VapeShop/ClientApp/src/components/Pages/Home/Home.js
+++ VapeShop/ClientApp/src/components/Pages/Home/Home.js
@@ -2,41 +2,36 @@ import React, { useContext } from 'react';
 import { useState } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap'
 
-import { PromotionCarousel } from './PromotionCarousel'
-import { CollectionButtonGroup } from './CollectionButtonGroup'
-import { CategoryButtonGroup } from './CategoryButtonGroup'
-import { ProductsMesh } from './ProductsMesh'
+import { PromotionCarousel } from './PromotionCarousel';
+import { CategoryButtonGroup } from './CategoryButtonGroup';
+import { ProductsMesh } from './ProductsMesh';
 import { Resource } from '../../utilities/Resource';
 import { Loadable } from '../../utilities/Loadable';
 
-import { ApiContext } from '../../../contexts/ApiContext'
+import { ApiContext } from '../../../contexts/ApiContext';
 
 const Home = (props) => {
   const allCategory = 'All products';
   const api = useContext(ApiContext);
 
   const [category, setCategory] = useState(allCategory);
-  const [collection, setCollection] = useState(null);
 
   console.log(api)
   return (
     <Resource path={api.products}>
       {({ payload, loading }) => {
-        let { products, categories, collections } = payload;
+        let { products, categories } = payload;
         return (
           <section>
             <PromotionCarousel />
             <br />
-            <Loadable>
-              <CollectionButtonGroup setCollection={setCollection} collections={collections} />
-            </Loadable>
-            <Grid container direction="row">
-              <Col>
+            <Grid fluid>
+              <Col sm={12} md={2}>
                 <Loadable>
                   <CategoryButtonGroup setCategory={setCategory} categories={categories && [allCategory, ...categories] || []} />
                 </Loadable>
               </Col>
-              <Col>
+              <Col sm={12} md={10}>
                 <Loadable loading={loading}>
                   <ProductsMesh products={products} category={category} />
                 </Loadable>
