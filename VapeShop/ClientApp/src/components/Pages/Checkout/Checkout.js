1a1aef8d667e4e04e9008df2aab4cbd7fd1b2214
Raff <rafal.lazicki99@gmail.com>
2021-03-11 13:31:40 +0100

    unifying styling

--- VapeShop/ClientApp/src/components/Pages/Checkout/Checkout.js
+++ VapeShop/ClientApp/src/components/Pages/Checkout/Checkout.js
@@ -3,20 +3,25 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import { Breadcrumbs, Typography } from '@material-ui/core';
 
+import { Text, PageSection, PageTitle, ListEntry, Button } from '../../utilities/ThemeComponents'
+
 import { CheckoutForm } from './CheckoutForm'
 import { CheckoutSummary } from './CheckoutSummary'
 
 const Checkout = () => {
     return (
         <Grid fluid className="p-0 mx-4">
-            <Col md={12} lg={6} >
-                <Row className="ml-2">
-                    <Breadcrumbs>
-                        <Link to='/Cart'>Cart</Link>
-                        <Typography >Shipping</Typography>
-                    </Breadcrumbs>
+            <Col md={12} lg={6} className="px-4" >
+                <Row className="ml-4">
+                    <PageTitle>Checkout</PageTitle>
+                    <ListEntry>
+                        <Breadcrumbs>
+                            <Link to='/Cart'>Cart</Link>
+                            <Typography >Shipping</Typography>
+                        </Breadcrumbs>
+                    </ListEntry>
                 </Row>
-                <Row className="ml-2">
+                <Row className="ml-4 pt-3">
                     <CheckoutForm />
                 </Row>
             </Col>
