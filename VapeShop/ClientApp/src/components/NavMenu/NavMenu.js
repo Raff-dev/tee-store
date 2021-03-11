4e33fb970561c2b9c53fb118f708df0f1be36f80
Raff <rafal.lazicki99@gmail.com>
2021-03-11 13:32:32 +0100

    cart notification badge

--- VapeShop/ClientApp/src/components/NavMenu/NavMenu.js
+++ VapeShop/ClientApp/src/components/NavMenu/NavMenu.js
@@ -4,10 +4,17 @@ import { Link } from 'react-router-dom';
 import { Navbar, Glyphicon } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav'
 
+import Badge from '@material-ui/core/Badge';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
 import { CartContext } from '../../contexts/CartContext'
+import { theme } from '../../contexts/ThemeContext';
 
 const NavMenu = () => {
     const cart = useContext(CartContext);
+
 
     return (
         <MyNav fixed="top">
@@ -20,13 +27,33 @@ const NavMenu = () => {
             </Brand>
 
             <Nav.Item>
-                <Nav.Link as={Link} to="/Cart" >
-                    <Glyphicon glyph='shopping-cart' /> Cart {cart.items.length}
+                <Nav.Link as={Link} style={{ color: theme.textInfo }} to="/Cart" >
+                    <StyledBadge badgeContent={cart.items.length} color="secondary">
+                        <ShoppingCartIcon />
+                    </StyledBadge>
                 </Nav.Link>
             </Nav.Item>
         </MyNav>
     );
 }
+const StyledBadge = styled(Badge)`
+    border: 5px solid;
+    border-color: ${props => theme.bgPrimary};
+    padding: 0 4px;
+`;
+
+const Counter = styled.span`
+    transform: translate(10px, 10px);
+    position: absolute;
+    background: ${props => theme.btnPrimary};
+    border-radius: 100%;
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    color: white;
+    font-weight:600;
+`;
+
 
 const Brand = styled(Navbar.Brand)`
     user-select:none;
