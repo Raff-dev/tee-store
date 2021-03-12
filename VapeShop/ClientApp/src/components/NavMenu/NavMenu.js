08d7dd11edd67c2af11c0cec4e675c3785174435
Raff <rafal.lazicki99@gmail.com>
2021-03-12 15:55:11 +0100

    cart notification badge

--- VapeShop/ClientApp/src/components/NavMenu/NavMenu.js
+++ VapeShop/ClientApp/src/components/NavMenu/NavMenu.js
@@ -5,8 +5,6 @@ import { Navbar, Glyphicon } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav'
 
 import Badge from '@material-ui/core/Badge';
-import { withStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 import { CartContext } from '../../contexts/CartContext'
@@ -14,7 +12,6 @@ import { theme } from '../../contexts/ThemeContext';
 
 const NavMenu = () => {
     const cart = useContext(CartContext);
-
 
     return (
         <MyNav fixed="top">
@@ -28,36 +25,19 @@ const NavMenu = () => {
 
             <Nav.Item>
                 <Nav.Link as={Link} style={{ color: theme.textInfo }} to="/Cart" >
-                    <StyledBadge badgeContent={cart.items.length} color="secondary">
+                    <Badge badgeContent={cart.items.length} color="secondary">
                         <ShoppingCartIcon />
-                    </StyledBadge>
+                    </Badge>
                 </Nav.Link>
             </Nav.Item>
         </MyNav>
     );
 }
-const StyledBadge = styled(Badge)`
-    border: 5px solid;
-    border-color: ${props => theme.bgPrimary};
-    padding: 0 4px;
-`;
-
-const Counter = styled.span`
-    transform: translate(10px, 10px);
-    position: absolute;
-    background: ${props => theme.btnPrimary};
-    border-radius: 100%;
-    width: 20px;
-    height: 20px;
-    text-align: center;
-    color: white;
-    font-weight:600;
-`;
-
 
 const Brand = styled(Navbar.Brand)`
     user-select:none;
 `;
+
 const MyNav = styled(Navbar)`
     position:sticky;
     margin:0;
@@ -65,7 +45,6 @@ const MyNav = styled(Navbar)`
     border: 0 solid #e5e7eb;
     background-color:transparent;
     background-image:none;
-
 `;
 
 export default NavMenu;
