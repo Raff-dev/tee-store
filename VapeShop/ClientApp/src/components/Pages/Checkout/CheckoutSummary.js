824f74831a14f6934c7dd24d929ba9afb30cec76
Raff <rafal.lazicki99@gmail.com>
2021-03-14 10:51:37 +0100

    checkout Summary styling

--- VapeShop/ClientApp/src/components/Pages/Checkout/CheckoutSummary.js
+++ VapeShop/ClientApp/src/components/Pages/Checkout/CheckoutSummary.js
@@ -7,43 +7,91 @@ import { ApiContext } from '../../../contexts/ApiContext'
 import { CartContext } from '../../../contexts/CartContext'
 
 import { Resource } from '../../utilities/Resource'
+import Badge from '@material-ui/core/Badge';
+import { theme } from '../../../contexts/ThemeContext'
+import { Loadable } from '../../utilities/Loadable';
 
 export const CheckoutSummary = () => {
     const api = useContext(ApiContext);
     const cart = useContext(CartContext);
-    const ids = { ids: Object.keys(cart.quantityMap) };
+
 
     return (
-        <Resource path={api.cartProducts} data={ids} disabled={!!ids['ids'] === false}>
+        <Resource path={api.cartProducts} data={{ ids: cart.ids }}>
             {({ payload, loading }) => {
+                const subtotal = cart.subtotal(payload);
+                const total = Math.round((cart.shipping + subtotal) * 100) / 100;
+
                 return (
-                    <section>
+                    <section className="px-4">
                         {payload.map((product, index) =>
-                            <ListEntry>
+                            <ListEntry key={index}>
                                 <ProductDisplay>
-                                    <ProductImage src={api.baseUrl + product.image} />
-                                    <div>
-                                        <p>{product.collection}</p>
-                                        <p>{product.title}</p>
-                                        <p>{`${product.variant} - ${product.size_label}`}</p>
-                                        <p>{cart.quantityMap[product.id]}</p>
+                                    <div className="d-flex">
+                                        <div>
+                                            <StyledBadge badgeContent={cart.quantityMap[product.id]} color="primary">
+                                                <ProductImage src={api.baseUrl + product.image} />
+                                            </StyledBadge>
+                                        </div>
+                                        <div>
+                                            <p>{product.collection}</p>
+                                            <Text muted>
+                                                <p>{product.title}</p>
+                                                <p>{`${product.variant} - ${product.size_label}`}</p>
+                                            </Text>
+                                        </div>
                                     </div>
+                                    <p>
+                                        <Text className="font-size-bold pr-4">{cart.currency}{product.price * cart.quantityMap[product.id]}</Text>
+                                    </p>
                                 </ProductDisplay>
 
                             </ListEntry>
                         )}
+                        <h4>
+                            <ListEntry>
+                                <Text >Shipping Method</Text>
+                                <Loadable loading={loading}>
+                                    <Text info>Delivery</Text>
+                                </Loadable>
+                            </ListEntry>
+                            <ListEntry>
+                                <Text >Subtotal</Text>
+                                <Loadable loading={loading}>
+                                    <Text info>{cart.currency}{subtotal}</Text>
+                                </Loadable>
+                            </ListEntry>
+                            <ListEntry>
+                                <Text >Shipping</Text>
+                                <Loadable loading={loading}>
+                                    <Text info>{cart.currency}{cart.shipping}</Text>
+                                </Loadable>
+                            </ListEntry>
+                            <ListEntry>
+                                <Text className="font-weight-bold">Total</Text>
+                                <Loadable loading={loading}>
+                                    <Text className="font-weight-bold">{cart.currency}{total}</Text>
+                                </Loadable>
+                            </ListEntry>
+                        </h4>
                     </section>
                 );
             }}
         </Resource>
     );
 };
+const StyledBadge = styled(Badge)`
+    background: #333 !important;
+    background-color: #333 !important;
+`;
 
 const ProductImage = styled.img`
-    width:100px;
+    width:70px;
 `;
 const ProductDisplay = styled.div`
     display: flex;
+    justify-content: space-between;
+    width:100%;
 `;
 
 
