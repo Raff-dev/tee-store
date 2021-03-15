6c53d3f01f19362cbba4910edf9c1ffc8511d3de
Raff <rafal.lazicki99@gmail.com>
2021-03-15 14:21:30 +0100

    add different component on empty cart

--- VapeShop/ClientApp/src/components/Pages/Checkout/Checkout.js
+++ VapeShop/ClientApp/src/components/Pages/Checkout/Checkout.js
@@ -1,34 +1,53 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import { Breadcrumbs, Typography } from '@material-ui/core';
 
-import { Text, PageSection, PageTitle, ListEntry, Button } from '../../utilities/ThemeComponents'
+import { CartContext } from '../../../contexts/CartContext'
+import { ApiContext } from '../../../contexts/ApiContext'
+
+import { PageTitle, ListEntry, PageSection } from '../../utilities/ThemeComponents'
 
 import { CheckoutForm } from './CheckoutForm'
 import { CheckoutSummary } from './CheckoutSummary'
+import { CartEmpty } from '../Cart/EmptyCart';
+import { Resource } from '../../utilities/Resource';
 
 const Checkout = () => {
+    const api = useContext(ApiContext);
+    const cart = useContext(CartContext);
+
     return (
-        <Grid fluid className="p-0 mx-4">
-            <Col md={12} lg={6} className="px-4" >
-                <Row className="ml-4">
-                    <PageTitle>Checkout</PageTitle>
-                    <ListEntry>
-                        <Breadcrumbs>
-                            <Link to='/Cart'>Cart</Link>
-                            <Typography >Shipping</Typography>
-                        </Breadcrumbs>
-                    </ListEntry>
-                </Row>
-                <Row className="ml-4 pt-3">
-                    <CheckoutForm />
-                </Row>
-            </Col>
-            <Col md={12} lg={6} style={{ "background-color": "#0001", "height": "1000px" }}>
-                <CheckoutSummary />
-            </Col>
-        </Grid>
+        <PageSection>
+            <Resource path={api.cartProducts} data={{ ids: cart.ids }}>
+                {({ payload, loading }) => {
+                    if (!loading && payload.length === 0) {
+                        return <CartEmpty />
+                    }
+                    return (
+                        <Grid fluid className="p-0 mx-md-4">
+                            <Col md={12} lg={6} className="px-4" >
+                                <Row className="ml-md-4">
+                                    <PageTitle>Checkout</PageTitle>
+                                    <ListEntry>
+                                        <Breadcrumbs>
+                                            <Link to='/Cart'>Cart</Link>
+                                            <Typography >Shipping</Typography>
+                                        </Breadcrumbs>
+                                    </ListEntry>
+                                </Row>
+                                <Row className="ml-md-4 pt-3">
+                                    <CheckoutForm />
+                                </Row>
+                            </Col>
+                            <Col md={12} lg={6} >
+                                <CheckoutSummary cartProducts={payload} loading={loading} />
+                            </Col>
+                        </Grid>
+                    );
+                }}
+            </Resource>
+        </PageSection>
     );
 }
 
