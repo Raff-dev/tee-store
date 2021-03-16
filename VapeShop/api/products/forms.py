38fd2cf43b1111b06ee9ddece0573d96302d8a73
Raff <rafal.lazicki99@gmail.com>
2021-03-16 22:06:08 +0100

    create product form

---
+++ VapeShop/api/products/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Category, Collection, Product, Size, Variant, Image
+
+
+class ProductForm(forms.ModelForm):
+    variant_name = forms.CharField()
+    image = forms.ImageField()
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def save(self, commit=True):
+        self.instance.variant_name = self.cleaned_data['variant_name']
+        self.instance.image = self.cleaned_data['image']
+        return super().save(commit=commit)
