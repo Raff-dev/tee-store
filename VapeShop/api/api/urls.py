fa058c1b7094e96852d4f1169aa757449a8c0e1d
Raff <rafal.lazicki99@gmail.com>
2021-03-20 09:02:09 +0100

    Update urls.py

--- VapeShop/api/api/urls.py
+++ VapeShop/api/api/urls.py
@@ -4,22 +4,19 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import static
 from django.conf import settings
-from products.views import CollectionViewSet, ProductViewSet, CategoryViewSet, CartViewSet
+from products.views import ProductViewSet
+from orders.views import OrderViewSet
 
 router = DefaultRouter()
-router.register('Categories', CategoryViewSet)
-router.register('Collections', CollectionViewSet)
 router.register('Products', ProductViewSet)
-router.register('Cart', CartViewSet)
+router.register('Orders', OrderViewSet)
 
 # The API URLs are now determined automatically by the router.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include((router.urls, 'Categories'), namespace='Categories')),
-    path('api/', include((router.urls, 'Collections'), namespace='Collections')),
     path('api/', include((router.urls, 'Products'), namespace='Products')),
-    path('api/', include((router.urls, 'Cart'), namespace='Cart')),
+    path('api/', include((router.urls, 'Orders'), namespace='Orders')),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
 
