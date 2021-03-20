7bd63efbfa59e80a48fea94c61d1129bf90dc01f
Raff <rafal.lazicki99@gmail.com>
2021-03-20 09:00:31 +0100

    remove unused view sets, move payment view to orders app

--- VapeShop/api/products/views.py
+++ VapeShop/api/products/views.py
@@ -1,14 +1,10 @@
-from functools import reduce
-import json
-from django.conf import settings
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
 from .models import Category, Product, Collection, Size
-from .serializers import CollectionSerializer, ProductListSerializer, ProductDetailSerializer, CategorySerializer
+from .serializers import ProductListSerializer, ProductDetailSerializer
 from .cart_serializers import CartSerializer
-import stripe
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
@@ -21,54 +17,19 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
             'collections': Collection.objects.values_list('name', flat=True),
             'products': serializer.data,
         }
-        return Response(data)
+        return Response(data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = ProductDetailSerializer(instance)
-
-        return Response(serializer.data)
-
-
-class CartViewSet(viewsets.GenericViewSet):
-    queryset = Size.objects.all()
-    serializer_class = CartSerializer
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(methods=['POST'], detail=False)
-    def cart_products(self, request, *args, **kwargs):
+    def cart(self, request, *args, **kwargs):
         try:
             ids = list(map(lambda id: int(id), request.data['ids']))
             sizes = Size.objects.filter(id__in=ids)
             serializer = CartSerializer(sizes, many=True)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except KeyError:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    @action(methods=['POST'], detail=False)
-    def create_payment_session(self, request, *args, **kwargs):
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        try:
-            ids = [int(id) for id in request.data.keys()]
-            sizes = Size.objects.filter(id__in=ids)
-            amount = int(reduce(lambda total, size: total+size.price*request.data[str(size.id)]*100, sizes, 0))
-            intent = stripe.PaymentIntent.create(
-                amount=amount,
-                currency='eur',
-                payment_method_types=['p24'],
-            )
-            result = {'clientSecret': intent['client_secret']}
-            return Response(result, status.HTTP_202_ACCEPTED)
-        except KeyError:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    lookup_field = 'name'
-
-
-class CollectionViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Collection.objects.all()
-    serializer_class = CollectionSerializer
-    lookup_field = 'name'
