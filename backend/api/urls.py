cf1b6b2282fddd31e479e104c487e5fc04876be3
Raff <rafal.lazicki99@gmail.com>
2021-04-18 17:29:52 +0200

    fix imports, remove help print

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.routers import DefaultRouter
+
 from products.views import ProductViewSet
 from orders.views import OrderViewSet
 
@@ -20,6 +21,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-for url in router.urls:
-    print(url)
