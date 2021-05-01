6f686d76d7d9e8896fca961a1490c31dd28a52fe
Raff <rafal.lazicki99@gmail.com>
2021-05-01 11:43:22 +0200

    move publishable key to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,13 +18,14 @@ import NavMenu from "./components/NavMenu/NavMenu";
 
 import { CartProvider } from "./contexts/CartContext";
 import { ApiProvider } from "./contexts/ApiContext";
-import { publishableKey } from "./secret";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
-const stripePromise = loadStripe(publishableKey);
+const stripePromise = loadStripe(
+    "pk_test_51ISJV9JbP5kYqOPtlSYCWM6016g6nuDnpTPouQdFXLc2pU1bOc5Kh6dr3UvUFkT9oxtWRvvADzqKwoeg64ndZ67R00clEPR6r2"
+);
 
 ReactDOM.render(
     <React.StrictMode>
